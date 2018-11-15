7ca35f9938cd1b897ee8c118478d267359326d33
hyunvinci <hyunvinci@gmail.com>
2018-11-15 16:06:29 +0900

    HeartMadeFirst FrontEnd commit

---
+++ server.js
@@ -0,0 +1,26 @@
+// 모듈을 추출합니다.
+var express = require('express');
+var path = require('path');
+
+//서버를 생성합니다.
+var app = express();
+
+var port = 80;
+
+// // request 이벤트 리스너를 설정합니다.
+// app.use(function (request, response, next) {
+//     // 변수를 선언합니다.
+//     var name = request.query.name;
+//     var region = request.query.region;
+//
+//     //응답합니다.
+//     // 아이디 + 지역 등을 이용하고 이 미들웨어를 좀 사용하면 어떨까요??
+//     response.send('<h1>'+name+'-'+region+'</h1>');
+// });
+
+app.use(express.static(path.join(__dirname, 'gallery')));
+
+// 서버를 실행합니다.
+app.listen(port, function(){
+    console.log("Server Running at http://127.0.0.1:"+`${port}`);
+});
